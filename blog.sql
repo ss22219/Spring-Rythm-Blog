78f3e915c7b8d47e179a520f434cd350c6496825
ss22219 <ss22219@qq.com>
2013-10-02 07:25:39 +0800

    #1

---
+++ blog.sql
@@ -0,0 +1,164 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Oct 02, 2013 at 01:24 AM
+-- Server version: 5.5.25a
+-- PHP Version: 5.4.4
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article`
+--
+
+CREATE TABLE IF NOT EXISTS `article` (
+  `article_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `create_date` date NOT NULL,
+  `modify_date` date NOT NULL,
+  `comment_count` int(11) NOT NULL,
+  `browse_count` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`article_id`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `article`
+--
+
+INSERT INTO `article` (`article_id`, `title`, `content`, `create_date`, `modify_date`, `comment_count`, `browse_count`, `type`, `status`, `user_id`) VALUES
+(1, '第一张', '是的发撒旦发射点发撒旦法', '2013-10-01', '2013-10-16', 0, 0, 0, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE IF NOT EXISTS `category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `type` int(11) NOT NULL,
+  `article_count` int(11) NOT NULL,
+  `parent_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`category_id`),
+  KEY `parent_id` (`parent_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`category_id`, `name`, `type`, `article_count`, `parent_id`) VALUES
+(2, '未分类', 0, 0, NULL),
+(3, '子集', 0, 0, 2),
+(4, '子集2', 0, 0, 3),
+(5, '标记', 1, 0, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category_relation`
+--
+
+CREATE TABLE IF NOT EXISTS `category_relation` (
+  `category_id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  `category_relation` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`category_relation`),
+  KEY `category_id` (`category_id`),
+  KEY `article_id` (`article_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `category_relation`
+--
+
+INSERT INTO `category_relation` (`category_id`, `article_id`, `category_relation`) VALUES
+(2, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `setting`
+--
+
+CREATE TABLE IF NOT EXISTS `setting` (
+  `key` varchar(255) NOT NULL,
+  `value` text NOT NULL,
+  PRIMARY KEY (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `setting`
+--
+
+INSERT INTO `setting` (`key`, `value`) VALUES
+('allRegister', '1'),
+('articlePageSize', '10'),
+('blogDescription', '那一抹温暖的阳光，是我们永恒的向往。'),
+('blogTitle', '蓝色空间'),
+('defaultRole', '0'),
+('defaultStatus', '1'),
+('lastArticleCount', '10'),
+('webSite', 'GOOL.COM.CN'),
+('weekStart', '0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(50) NOT NULL,
+  `nice_name` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `picture` varchar(255) DEFAULT NULL,
+  `register_date` date NOT NULL,
+  `status` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `user_name`, `nice_name`, `password`, `picture`, `register_date`, `status`, `email`, `role`) VALUES
+(1, 'ss22219', '幻影gool', '123456', '', '2013-10-01', 1, 'ss22219@qq.com', 1);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `category`
+--
+ALTER TABLE `category`
+  ADD CONSTRAINT `category_ibfk_1` FOREIGN KEY (`parent_id`) REFERENCES `category` (`category_id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
